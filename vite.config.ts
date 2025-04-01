02ffef918257b35b00892dabd87940bf0ecbdf25
Yihao <yihao.zhuo.2001@gmail.com>
2025-04-01 09:31:52 +0200

    🚑 feat: add testing setup with vitest and configure docker-compose for development

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+		passWithNoTests: true,
+  },
 });
