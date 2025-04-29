8a6749a1ae65690e63542ce970c4895c608cd498
lucaribon <lucaribon0203@gmail.com>
2025-04-29 18:57:24 +0200

    fix: url fetchato dal bottone nuova chat;

--- src/lib/components/BottomNavBar.svelte
+++ src/lib/components/BottomNavBar.svelte
@@ -4,16 +4,15 @@
 	import { goto } from '$app/navigation';
 	let { data } = $props();
 	import { page } from '$app/stores';
-
+	import { PUBLIC_DATABASE_URL } from '$env/static/public';
 
 	async function newChat() {
-		const chat_id = await fetch('http://localhost:8000/chats/new_chat', {
-			method: 'GET',
+		const chat_id = await fetch('http://' + PUBLIC_DATABASE_URL + '/chats', {
+			method: 'POST',
 			headers: {
 				Authorization: 'Bearer ' + data.token
 			}
 		}).then((res) => res.json());
-
 
 		await invalidate('app:chat'); // per rimuovere la cache, altrimenti la lista di chat non si aggiorna
 		await goto(`/chat/${chat_id.chat_id}`);
@@ -22,7 +21,7 @@
 	let isHome = $page.url.pathname === '/';
 </script>
 
-<footer class="flex justify-around bg-white shadow-md pb-6 pt-2">
+<footer class="flex justify-around bg-white pb-6 pt-2 shadow-md">
 	<a href="/" class="">
 		<div
 			class="flex flex-col items-center gap-1 rounded-full p-2 px-4 text-sm {!isHome &&
@@ -35,9 +34,7 @@
 	<div>
 		<button onclick={newChat} class="flex flex-col items-center gap-1">
 			<div class="absolute bottom-5 flex flex-col items-center justify-center text-center text-sm">
-				<div
-					class="rounded-full item-primary p-5 transition duration-150 ease-in"
-				>
+				<div class="item-primary rounded-full p-5 transition duration-150 ease-in">
 					<MessageCirclePlus />
 				</div>
 				<p>Nuova chat</p>
