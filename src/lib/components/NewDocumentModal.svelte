f11627c1cba0116334a1ec03a881c216d631505c
lucaribon <lucaribon0203@gmail.com>
2025-05-04 15:48:40 +0200

    fix: rimosso autore da aggiunta documento;

--- src/lib/components/NewDocumentModal.svelte
+++ src/lib/components/NewDocumentModal.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
-    
-    
+
 	let name = '';
 	let author = '';
 
@@ -14,32 +13,51 @@
 		});
 	}
 
-    let files : any, fileInput;
+	let files: any, fileInput;
 </script>
 
-<div class="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-	<div class="bg-white rounded-xl p-6 w-[90%] max-w-md shadow-xl">
-		<h2 class="text-lg font-semibold mb-4">Inserimento File</h2>
+<div class="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+	<div class="w-[90%] max-w-md rounded-xl bg-white p-6 shadow-xl">
+		<h2 class="mb-4 text-lg font-semibold">Inserimento File</h2>
 
 		<div class="mb-3">
-			<input type="text" bind:value={name} placeholder="Nome del documento" required class="w-full pl-10 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white" />
+			<input
+			id="name"	
+			type="text"
+				bind:value={name}
+				placeholder="Nome del documento"
+				required
+				class="w-full rounded-lg border border-gray-300 bg-white py-2 pl-10 focus:outline-none focus:ring-2 focus:ring-blue-500"
+			/>
 		</div>
 		<div class="mb-3">
-			<input type="text" bind:value={author} placeholder="Autore" required class="w-full pl-10 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white" />
-		</div>
-        <div class="mb-3">
-            <label for="file">Seleziona uno o più file</label>
-            <input  bind:value={files} bind:this={fileInput} id="file" type="file" accept=".txt, .pdf" required multiple class="w-full pl-10 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white" />
+			<label for="file">Seleziona uno o più file</label>
+			<input
+				bind:value={files}
+				bind:this={fileInput}
+				id="file"
+				type="file"
+				accept=".txt, .pdf"
+				required
+				multiple
+				class="w-full rounded-lg border border-gray-300 bg-white py-2 pl-10 focus:outline-none focus:ring-2 focus:ring-blue-500"
+			/>
 			{#if files && files[0]}
-			    <p>
-		        {files[0].name}
-	            </p>
-            {/if}
-        </div>
+				<p>
+					{files[0].name}
+				</p>
+			{/if}
+		</div>
 
 		<div class="flex justify-end space-x-2">
-			<button class="px-4 py-2 rounded-lg bg-gray transition duration-150 ease-in" onclick={() => dispatch('cancel')}>Annulla</button>
-			<button class="px-4 py-2 rounded-lg item-primary transition duration-150 ease-in" onclick={submitForm}>Salva</button>
+			<button
+				class="bg-gray rounded-lg px-4 py-2 transition duration-150 ease-in"
+				onclick={() => dispatch('cancel')}>Annulla</button
+			>
+			<button
+				class="item-primary rounded-lg px-4 py-2 transition duration-150 ease-in"
+				onclick={submitForm}>Salva</button
+			>
 		</div>
 	</div>
 </div>
