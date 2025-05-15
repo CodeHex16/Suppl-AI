26d85544131c0ac5ebd1e918de4d5054a53e0510
Yihao <yihao.zhuo.2001@gmail.com>
2025-05-15 21:50:43 +0200

    ✨ feat(ci): add CI workflow for testing and coverage reporting

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
 		reporters: 'verbose', 
 		coverage: {
       provider: 'v8',
-      reporter: ['text', 'html'],
+      reporter: ['text', 'html', 'lcov'],
       reportsDirectory: './coverage',
       exclude: [
 				'src/setupTests.ts', 
